a54e99aac1220eb7adcd302d69bebfd0e27a63b7
shaown <smshaown65@gmail.com>
2021-06-29 20:37:43 +0600

    resrved world camel case to words

---
+++ #3-reservedWords.js
@@ -0,0 +1,14 @@
+// https://www.w3schools.com/js/js_reserved.asp   <----- ||reserved world
+
+
+// 1. example reserved world 
+
+// const else = 'something'
+// console.log(else)
+
+
+// 2.  example javascript #camel case to words
+
+var mathNumber = 10
+var accountNumberDetailsId = 104
+var account_number_details_id = 105
