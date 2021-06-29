70666f404d17a921f1254878d525055c09f03706
shaown <smshaown65@gmail.com>
2021-06-29 20:20:17 +0600

    variable declare

---
+++ #2-variables.js
@@ -0,0 +1,7 @@
+// Variables = var  **Declare kora hoy
+var name = 's m shaown'
+var learning = 'and 8 month'
+var age = 21
+
+console.log(name + ' my name')
+console.log('I am learnog web design and development' + learning)
