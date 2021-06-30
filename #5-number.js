0daeba1a8127e02b2f8ea622f20494f8499544d6
shaown <smshaown65@gmail.com>
2021-06-30 17:02:19 +0600

    javascript number

---
+++ #5-number.js
@@ -0,0 +1,24 @@
+
+// ****************Number**********
+
+// Number 
+// //10 4.5 8.12 3.14
+// String 
+// 'this is a text' + 'This is also text'
+
+
+
+// Javascript is Case Sensitve 
+// Meanig, var and VAR or Var is not smae
+
+
+var n = 1451
+var f = 3.14
+var nn = Number('45.45')
+console.log(nn)
+console.log(Number.parseFloat(nn))
+console.log(Number.parseInt(nn))
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_SAFE_INTEGER)
+console.log(1 / 0)
+console.log('abc' * 10)
