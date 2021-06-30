a013aad4d4e8ef80f1421e770e42b9b8c510d4d1
shaown <smshaown65@gmail.com>
2021-06-30 20:10:46 +0600

    Boolean Number

---
+++ #6-booleans.js
@@ -0,0 +1,13 @@
+
+
+
+// We have to think everything String when it comes to solve problem using progamming language.
+
+
+  var b1 = true 
+  var b2 = false 
+
+  var b3 = Boolean(true)
+  var b4 = Boolean(false)
+
+  console.log(b3, b4)
