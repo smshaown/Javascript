ef730e889fdd272dfe536c781283d9007230125d
shaown <smshaown65@gmail.com>
2021-06-30 19:54:23 +0600

    String nioym

---
+++ #5-string.js
@@ -0,0 +1,13 @@
+// ***********String**********
+
+var str = 'String'
+var str2 = "String"
+var str3 = `String`
+
+
+var str4 = String('s m shaown')
+var str5 = String(156)
+var str6 = String(3.1416)
+
+
+console.log(str, str2, str3, str4, str5, str6)
