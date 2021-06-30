c685a7a169b9fcbaf39253087cd3c909c0ed5be5
shaown <smshaown65@gmail.com>
2021-06-30 16:04:39 +0600

    Data Type

---
+++ #4-dataType.js
@@ -0,0 +1,21 @@
+// primitive 
+// primitive types are defined by ja
+Number 
+//10 4.5 8.12 3.14
+String 
+'this is a text' + 'This is also text'
+
+Boolean
+true 
+false
+
+
+undefined
+null
+
+
+object 
+// object types are user defined data type
+Array 
+object 
+Function
