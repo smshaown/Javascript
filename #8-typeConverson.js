d3ed86378a8a2411fbc0ed385a5048a7cd3eb145
shaown <smshaown65@gmail.com>
2021-06-30 20:39:51 +0600

    basic Type converson

---
+++ #8-typeConverson.js
@@ -0,0 +1,33 @@
+var str = '1000'
+var n = 10
+
+console.log(Number.parseInt(str))
+console.log(n.toString())
+
+
+console.log(Boolean(-Infinity))
+
+// Falsy value 
+''
+0
+null 
+undefined 
+null
+
+console.log(Boolean(''))
+console.log(Boolean('fffaklsjf'))
+
+
+console.log(Boolean(null))
+console.log(Boolean(undefined))
+
+console.log(Boolean(0))
+console.log(Boolean(4511))
+
+
+
+
+console.log(true)
+var x = true
+console.log(x.toString())
+
