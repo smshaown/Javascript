0b1898ae6e6596ba8f3a1b545b75cb5edaee620b
shaown <smshaown65@gmail.com>
2021-07-01 08:10:35 +0600

    arithmentic || Assignment || Comparison || Logical || Typeof || Operator

--- #11-operators.js
+++ #11-operators.js
@@ -1,3 +1,8 @@
+
+
+// https://www.w3schools.com/jsref/jsref_operators.asp
+
+
 /*
 
 +	Addition	x = y + 2	y = 5	x = 7	
@@ -46,7 +51,6 @@ console.log(b--);
 /=	x /= y	x = x / y	x = 2	
 %=	x %= y	x = x % y	x = 0 
 
-*/
 
 var a = 10
 var b = 20
@@ -68,5 +72,66 @@ console.log(a)
 a %= b
 console.log(a)
 
+*/
 
 
+
+
+
+// Comparison Operators
+
+var a = 10
+var b = 20
+
+console.log(a == b) // false
+console.log(a != b) // true
+
+console.log(a > b) // false
+console.log(a < b ) // true
+
+a = 20
+
+console.log(a >= b) // true
+console.log(a <= b) // true
+
+var c = '50'
+var d = 50
+
+console.log(c == d)
+console.log(c === b)
+console.log(c !== b)
+
+
+
+
+// // Logical Operators
+// &&	and	(x < 10 && y > 1) is true	
+
+// ||	or	(x === 5 || y === 5) is false	
+
+// !	not	!(x === y) is true
+
+
+
+// JavaScript Bitwise Operators
+
+// Operator	Description	 
+// &	        AND	         
+// |	        OR	         
+// ~	        NOT	        
+// <<	        Left        
+// >>	        Right       
+
+
+
+// The typeof Operator
+typeof "John"                 // Returns string
+typeof 3.14                   // Returns number
+typeof NaN                    // Returns number
+typeof false                  // Returns boolean
+typeof [1, 2, 3, 4]           // Returns object
+typeof {name:'John', age:34}  // Returns object
+typeof new Date()             // Returns object
+typeof function () {}         // Returns function
+typeof myCar                  // Returns undefined (if myCar is not declared)
+typeof null 
