91f51d18a75d45b957a3e89feb123b98552ae889
shaown <smshaown65@gmail.com>
2021-07-01 13:02:20 +0600

    basic Math Function

---
+++ #13-mathFunction.js
@@ -0,0 +1,21 @@
+ 
+
+//  Function are core building blocks of any programming language. We will discuss about functions in details in future chapters. 
+
+console.log(Math.E) // vules hide thake oi tha ke  bole ***constant
+console.log(Math.PI)
+
+var n = 4.589
+
+ console.log(Math.abs(n))// Use Negative Number to Get Absolute Value
+ console.log(Math.floor(n))
+ console.log(Math.ceil(n))
+console.log(Math.round(n))
+console.log(Math.max(400, 500))
+console.log(Math.min(8, 5, 1, 100, 500))
+console.log(Math.pow(2, 5))
+console.log(Math.sqrt(3))
+console.log(Math.sqrt(64))
+
+
+console.log(Math.round(Math.random() * 50 + 1))
