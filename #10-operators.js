2dfc294b0ca1e32cda166966f9ed12259828a75a
shaown <smshaown65@gmail.com>
2021-07-01 06:42:54 +0600

    Aritmetic Operator

---
+++ #10-operators.js
@@ -0,0 +1,23 @@
+/*
+
+// Arithmetic Operator
+// + - * / % ++ --
+
+var a = 11
+var b = 6
+
+// console.log(a % b);
+
+//------> Incremental 2 set
+//  1. Pre Incremental - 2. Post Incremental
+
+// 1. Pre Incremental
+console.log(++a)
+console.log(--b)
+
+// 2. Post Incremental
+console.log(a++)  // ----------> a = a + 1
+console.log(b--);
+
+
+*/
