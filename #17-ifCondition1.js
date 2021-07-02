35eedd3b491734373043427b1d8db16e12a38b59
shaown <smshaown65@gmail.com>
2021-07-02 18:26:44 +0600

    if condition even & odd

---
+++ #17-ifCondition1.js
@@ -0,0 +1,22 @@
+var a = 10 
+var b = 20
+
+// a > b true false
+
+if(a > b){
+    console.log('A is greater than B')
+}
+
+if(a < b){
+    console.log('B is greater than A')
+}
+
+var n = 5
+
+if(n % 2 === 0){
+    console.log(n + ' is Even Number')
+}
+
+if(n % 2 === 1){
+    console.log(n + ' is Odd Number')
+}
