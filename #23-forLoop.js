5daf20defe7edb1df99403d787561e88e9fb802f
shaown <smshaown65@gmail.com>
2021-07-03 15:31:06 +0600

    for loop

---
+++ #23-forLoop.js
@@ -0,0 +1,29 @@
+// for(var i = 0; i<=100; i++){
+//     console.log(i);
+// }
+
+// odd || even number 
+/*
+for(var i = 0; i<=100; i++){
+    if(i % 2 === 0){
+        console.log(i)
+    }
+}
+*/
+
+
+// var sum = 0;
+// for(var i = 1; i<=10; i++){
+//     sum += i;
+//     console.log(sum)
+// }
+
+
+
+var sum = 0;
+for(var i = 1; i <= 10; i++){
+    console.log(sum +' + ' + i + ' = ' + (sum+i))
+    sum += i
+}
+
+console.log('result = ' + sum)
