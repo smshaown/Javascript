66cd006350602c6235b8358e904c301e47e11672
shaown <smshaown65@gmail.com>
2021-07-03 09:11:37 +0600

    Ternary Operator logial operation

---
+++ #26-ternaryOperator.js
@@ -0,0 +1,15 @@
+var n = 10
+var str = ''
+
+if(n % 2 ===  0){
+    str = 'EVEN'
+}else{
+    str = 'ODD'
+}
+
+console.log(str)
+
+// Ternary Operator
+
+var result = n % 2 === 0 ? 'EVEN' : 'ODD'
+console.log(result)
