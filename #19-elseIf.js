b5100330466410ca88288a27644a81acfbb7de69
shaown <smshaown65@gmail.com>
2021-07-03 06:46:00 +0600

    else if condition

---
+++ #19-elseIf.js
@@ -0,0 +1,19 @@
+var a = 20
+var b = 20
+
+if(a > b){
+    console.log('A is greater than B')
+}else if(a < b){
+    console.log('B is greater than A')
+}else{
+    console.log('They Both are same')
+}
+
+var n = 53 
+if(n === 0){
+    console.log(n + ' is zero')
+}else if(n % 2 === 0){
+    console.log(n + ' is Even Number')
+}else{
+    console.log(n + ' is odd number')
+}
