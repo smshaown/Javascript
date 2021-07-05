c63929633ec8a08402f350e5486681fb79d55adf
shaown <smshaown65@gmail.com>
2021-07-05 20:29:56 +0600

    Nested Loop

---
+++ #27-nestedLoop.js
@@ -0,0 +1,34 @@
+
+
+// Nested Loop
+
+// var n =5
+// for(var i = 1; i<=n; i++){
+//     var result = ''
+//     for(var j = 1; j<=i; j++){
+//         result += j + ' '
+//     }
+//     console.log(result)
+// }
+
+// var n = * 
+// for(var i = 1; i<=n; i++){
+//     var result = ''
+//     for(var j = 1; j<=i; j++ ){
+//         result += j + ''
+//     }
+// }
+
+// ******
+// ******
+// ******
+// ******
+
+var n = 10;
+for(var i = 0; i<=n; i++){
+    var result = ''
+    for(var j=1; j<=n; j++){
+        result += '* '
+    }
+    console.log(result)
+}
