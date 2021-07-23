5f5e33dc38844378c7abbed8a7e7c4bbd3e41e05
shaown <smshaown65@gmail.com>
2021-07-23 09:17:00 +0600

    Escape Notation

---
+++ #38-escape-notation.js
@@ -0,0 +1,15 @@
+/*What is Escape Notation in js
+
+##Note: যে সব ক্যারেক্টার গুলো প্রোগ্রামিং এ বা কোডে প্রাইভেট করা আছে ব্যবহার করা যাবে না সে সব ক্যারেক্টার গুলো যদি ব্যবহার করতে চাই স্ট্রিং আকারে সে ক্যারেক্টার গুলো সামনে ব্লেক ‘\something\’ */
+
+var str = 'This is a \'String\''
+console.log(str)
+
+var str2 = 'This is a \nString'
+console.log(str2)
+
+var str3 = 'This is a \tString'
+console.log(str3)
+
+var str4 = 'This is a \\String'
+console.log(str4)
