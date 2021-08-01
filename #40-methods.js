402e8965641bc35b8e5735efe4faa72a6cc89333
shaown <smshaown65@gmail.com>
2021-08-01 21:44:48 +0600

    javascript methods

--- #40-methods.js
+++ #40-methods.js
@@ -1,4 +1,45 @@
-var a = 'I am '
-var b = 'sm shaown'
-var c = a.concat(' ', b)
-console.log(c)
+        // concat()
+// var a = 'I am '
+// var b = 'sm shaown'
+// var c = a.concat(' ', b)
+// // console.log(c)
+
+
+//         Substr()
+// var a = 'I am '
+// var b = 'sm shaown'
+// var c = a.concat(' ', b)
+// var d = c.substr(8, 5)
+// console.log(d)
+
+        //    CharAt()
+// var a = 'I am'
+// var b = 'sm shaown'
+// var c = a.concat(' ', b)
+// console.log(c.charAt(5))
+// // ans: (s)
+
+//   startsWith()
+// var a = 'I am '
+// var b = 'SM shaown'
+// var c = a.concat(' ', b)
+// console.log(c.startsWith('I am'))
+// console.log(c.endsWith('SM shaown'))
+// 1 ans: true
+// 2 ans: true
+
+
+
+
+// toUpperCase
+// toLowerCase
+// var a = 'I am shaown'
+// var b = 'SHE HAS SUCCESS'
+// console.log(a.toUpperCase())
+// console.log(b.toLowerCase())
+// #1 ans: I AM SHAOWN
+// #2 ans: she has success
+
+//                 .trim()
+// console.log('       smshawoen       '.trim())
+// // ans:shmshaown
