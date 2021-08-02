7208f87bbdb2d93ef02e31b242d0048ea84b2c36
shaown <smshaown65@gmail.com>
2021-08-02 19:59:59 +0600

    Insert Remove Array

---
+++ #45-insertRemove.js
@@ -0,0 +1,20 @@
+// Array unshift
+// Array splice
+// Array Push
+
+// built-in methods
+
+var arr = [1, 2, 3, 4, 5, 6, 7, 8]
+// Insert 9 to index 3
+// arr[3] = 9
+// arr.push(9)
+// arr.push(10) last update hobe
+// arr.unshift(0) first push update  
+// arr.splice(5, 0, 9, 10)
+// remove Date
+// arr.pop()
+// arr.shift()
+// arr[3] = null // --> x 
+// arr.splice(8, 0, 9, 10, 11, 12, 13, 14, 15)
+// arr.splice(6, 2, 100)
+console.log(arr)
