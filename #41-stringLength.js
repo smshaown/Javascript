948b8c431a638a60f53d4eb9ff48a8a05f39a5a2
shaown <smshaown65@gmail.com>
2021-08-02 07:59:41 +0600

    string leangth

---
+++ #41-stringLength.js
@@ -0,0 +1,14 @@
+var str = 'Some String'
+
+var length = 0
+
+while(true){
+    if(str.charAt(length) == ''){
+        break
+    }else{
+        length++
+    }
+}
+console.log(length)
+console.log(str.length)
+console.log('some is worn man.'.length)
