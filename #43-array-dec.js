2a1f30050b02d8e5fdc04a3ee89d07c50dfa32a9
shaown <smshaown65@gmail.com>
2021-08-02 09:20:47 +0600

    Array Declaration

---
+++ #43-array-dec.js
@@ -0,0 +1,17 @@
+
+var arr = [1, 2, 3, 4, 5]
+console.log(arr)
+console.log(arr[1])
+// Data add ? entry
+arr[5] = 6
+arr[10] = 11
+console.log(arr)
+// Array length
+console.log(arr.length)
+// Array Update
+arr [2] = 23
+console.log(arr)
+
+
+arr2 = Array(1, 2, 3)
+console.log(arr2)
