3f4a49162cac47a592c6b9c3142afeceba1de6c7
shaown <smshaown65@gmail.com>
2021-08-04 08:36:32 +0600

    Object Literal vs Constructo

---
+++ #51-objectLiteraVSconstructo.js
@@ -0,0 +1,27 @@
+
+// #obj{}
+// obj.x = 10
+
+// console.log(typeof obj)
+// console.log(obj)
+
+
+// Object Literal 
+
+var point = {
+    x: 10,
+    y: 20,
+}
+point.z = 30
+point.a = 40
+console.log(point)
+// ans:{ x: 10, y: 20, z: 30, a: 40}
+
+// Object Constructo
+var obj = Object()
+obj.a = 10
+console.log(obj)
+
+var obj2 = new Object()
+obj2.b = 20 
+console.log(obj2)
