796db4dc755b3e086265a61b565d29f1be39ac5c
shaown <smshaown65@gmail.com>
2021-08-04 10:00:23 +0600

    Object Properties updated

---
+++ #54-setting-properties.js
@@ -0,0 +1,20 @@
+
+
+
+var point = {
+    x: 10, 
+    y:20
+}
+
+point.x = 20 
+point.a = 30
+console.log(point)
+// console.log(point.d)
+
+// Array Updated
+
+point['y'] = 100
+
+var prop = 'z'
+point[prop] = 55
+console.log(point)
