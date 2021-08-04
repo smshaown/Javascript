58159c3cf8c91de06237d817b6c44993b822a17c
shaown <smshaown65@gmail.com>
2021-08-04 09:41:10 +0600

    #53-accessing object properties

---
+++ #53-accessing-object-properties.js
@@ -0,0 +1,16 @@
+
+// ....notation
+var point = {
+    x: 10,
+    y: 20,
+    z: 30
+}
+console.log(point.x)
+console.log(point.y)
+
+// array notation
+console.log(point['x'])
+console.log(point['z'])
+
+var show = 'z'
+console.log(point[show])
