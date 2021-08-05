c5402776beacaeec03b9b178419d7b731020cd2e
shaown <smshaown65@gmail.com>
2021-08-05 07:39:35 +0600

    Invoking Function -> call kora

---
+++ #61-invoking-function.js
@@ -0,0 +1,11 @@
+// Invoking Function
+
+function functionName(){
+    console.log('I am Function')
+}
+
+functionName() // Invoking Function ba call kor
+
+for(var i = 0; i<=10; i++){
+     functionName()
+}
