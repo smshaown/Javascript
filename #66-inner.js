586adabee57b8ea7234497e31e66dda52873f14a
shaown <smshaown65@gmail.com>
2021-08-05 18:39:33 +0600

    Inner Function

---
+++ #66-inner.js
@@ -0,0 +1,18 @@
+
+// Inner Function
+
+function something(greet, name){
+    function getFirstName() {
+        if (name) {
+            return name.split(' ')[0]
+        }else {
+            return ''
+        }
+    }
+
+    var message = greet + ' ' + getFirstName()
+    console.log(message)
+
+}
+
+something('Good Morning', 'She was every Day ran')
