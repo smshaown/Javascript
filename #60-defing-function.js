5a99982bac5eb1ba55fbe025ec97de161aa88a07
shaown <smshaown65@gmail.com>
2021-08-05 07:19:48 +0600

    Defing Function

---
+++ #60-defing-function.js
@@ -0,0 +1,18 @@
+
+// Defining A function
+
+function definingName(){
+
+}
+
+function add(){
+    var a = 20,
+    var b = 30
+    console.log(a + b)
+}
+
+function sub(){
+    var a = 20,
+    var b = 30
+    console.log(a - b)
+}
