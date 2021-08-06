f15608e9b6d1353db2c7a76466e69363a53cc823
shaown <smshaown65@gmail.com>
2021-08-06 19:31:48 +0600

    Start Funtional Programming Chapter Nine

---
+++ #68-functiionalProgramming.js
@@ -0,0 +1,20 @@
+// Chapter Nine: Functional Programing with javascript
+
+// What is Functional programming 
+
+'Functional programming is a programming paradigm-a style of building the structure and elements of computer programs and avoids changing state and mutable data-wikipedia'
+
+//Three Main terms
+
+ /**
+   1. Pure Functions 
+   2. First Class Function 
+   3. Higher Order Functions
+
+
+
+********* Function Acts Like a value ************
+
+
+  */
+
