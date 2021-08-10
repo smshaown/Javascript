25c58a3046aefc5dee7114d3793f28a005d9a798
shaown <smshaown65@gmail.com>
2021-08-10 06:52:56 +0600

    Find Findindex.js

---
+++ #78-find-findec.js
@@ -0,0 +1,29 @@
+/* var arr = [7, 4, 8, 6, 9, 2, 1, 70, 3]
+
+var result = arr.find(function (value) {
+    return value === 9
+})
+
+console.log(result) */
+// ans: 9
+
+/* // Array index value
+var arr = [7, 4, 8, 6, 9, 2, 1, 70, 3]
+var result = arr.findIndex(function (value) {
+    return value === 9
+})
+console.log(result) */
+
+function myFind(arr, cd){
+    for(var i = 0; i< arr.length; i++) {
+        if(cb(arr[i])){
+            return i
+        }
+    }
+}
+
+var result = myFind(arr, function(value){
+    return value === 9
+})
+
+console.log(result)
