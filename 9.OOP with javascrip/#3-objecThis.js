c32f44cda1559b0e44d41f7f4f895d79efa5dd09
shaown <smshaown65@gmail.com>
2021-08-14 10:26:40 +0600

    this not completed

---
+++ 9.OOP with javascrip/#3-objecThis.js
@@ -0,0 +1,22 @@
+var rect = {
+    width: 100,
+    hight: 50,
+
+    draw: function() {
+        console.log('I am ra rectangle')
+        this.printProperties();
+    },
+    printProperties: function() {
+        console.log('My width is ' + this.width)
+        console.log('My height is ' + this.height)
+    }
+}
+
+
+function myFunc() {
+    console.this(this)
+}
+new myFunc()
+
+
+/// sob mahtar upor diye geche
